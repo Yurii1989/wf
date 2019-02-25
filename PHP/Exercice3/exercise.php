3445c06400cc8afc0dd6924b79db74c1fcfcc974
yurii1989 <nlyuriinl@gmail.com>
2019-02-25 16:46:01 +0100

    changes to exercise 3

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -1,4 +1,18 @@
 <?php
 
 $input;
-$winner;
+$bortherA = 0;
+$brotherB = 0;
+foreach ($input as $game) {
+        if ($game[0]>$game[1]) {
+            $bortherA++;
+        } else if ($game[0]<$game[1]) {
+            $brotherB++;
+        }
+        else {};
+}
+if ($bortherA>$brotherB) {
+    echo $winner = 'A';
+    } else {
+    echo $winner = 'B';
+}
