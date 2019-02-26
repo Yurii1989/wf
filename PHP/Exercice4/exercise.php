5446c6f4dbaaf2aff68d264613d07666b6bd3bef
yurii1989 <nlyuriinl@gmail.com>
2019-02-26 10:56:26 +0100

    exercise 4 corrections

--- PHP/Exercice4/exercise.php
+++ PHP/Exercice4/exercise.php
@@ -1,14 +1,13 @@
 <?php
 
-function getAllMondaysOfMonth($year, $month)
-{
-    $mondays = [];
-    $date = new DateTime('first monday of '.(DateTime::createFromFormat('Yn', $year.$month))->format('F Y'));
-    $interval = DateInterval::createFromDateString('next monday');
-    while($date->format('m') == $month) {
-        $mondays[] = $date->format('l j, M Y');
-        $date->add($interval);
+function getAllMondaysOfMonth($year, $month) {
+    $calendar = [];
+    $dateformat = new DateTime('first monday of '.DateTime::createFromFormat('Y-n', $year.'-'.$month)->format('F Y'));
+    $interval = DateInterval::createFromDateString('7 days');
+    while ($dateformat->format('n') == $month) {
+        $calendar [] = $dateformat->format('l j, M Y');
+        $dateformat->add($interval);
     }
-    return $mondays;
-    }
+    return $calendar;
 
+}
