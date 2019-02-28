23e3e126f87e5371acf88499620e47307f39a358
yurii1989 <nlyuriinl@gmail.com>
2019-02-28 15:28:13 +0100

    changes to ex7

--- PHP/Exercice7/exercise.php
+++ PHP/Exercice7/exercise.php
@@ -1,1 +1,20 @@
 <?php
+
+function divide(int $value, int $divisor) {
+  if ($divisor === 0) {
+    throw new RuntimeException("Division by 0 is not allowed");
+  }
+    return $value / $divisor;
+};
+
+function arrayDivide(array $valToDiv, int $divisor):array {
+    $result = [];
+   foreach ($valToDiv as $val) {
+       try {
+           $result[] = divide($val, $divisor);
+       } catch (RuntimeException $exception) {
+           $result[] = $val;
+       }
+   }
+    return $result;
+}
