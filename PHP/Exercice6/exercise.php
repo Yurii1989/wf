b664d2efaa962941b17957149f947e63a7623f5a
yurii1989 <nlyuriinl@gmail.com>
2019-02-28 12:27:57 +0100

    changes to exercise 6

--- PHP/Exercice6/exercise.php
+++ PHP/Exercice6/exercise.php
@@ -1,1 +1,11 @@
 <?php
+
+function easterReverse($filePath) {
+    $fileContent = file_get_contents($filePath);
+    $secondPart = substr($fileContent, floor(strlen($fileContent) / 2));
+    $firstPart = substr($fileContent, 0 , strlen($secondPart) - 1);
+    $file = fopen($filePath, 'r+');
+    fseek($file, strlen($firstPart), SEEK_SET);
+    fwrite($file, strrev($secondPart), strlen($secondPart));
+    fclose($file);
+};
